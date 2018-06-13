0690853c93760e179aea7b5c80df28471baf5a4d
Dolev <dolevi101@gmail.com>
2018-06-13 15:04:02 +0300

    changed to empty project

--- dly_web_app/dly_web_app/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace dly_web_app.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- dly_web_app/dly_web_app/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dly_web_app.MvcApplication" Language="C#" %>
