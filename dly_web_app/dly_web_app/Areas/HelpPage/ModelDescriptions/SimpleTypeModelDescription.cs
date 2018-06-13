a88f879a4a14aee3c0a40f34249008f2da74f605
Dolev <dolevi101@gmail.com>
2018-06-13 14:56:08 +0300

    first commit

---
+++ dly_web_app/dly_web_app/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace dly_web_app.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ dly_web_app/dly_web_app/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dly_web_app.MvcApplication" Language="C#" %>
