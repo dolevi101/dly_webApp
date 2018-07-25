9a9ce4bf9ebec5d336670e05cd65365e16f2dd7c
Dolev <dolevi101@gmail.com>
2018-07-25 15:46:28 +0300

    login page

---
+++ dly_web_app_2/dly_web_app_2/JavaScript.js
@@ -0,0 +1,19 @@
+﻿function login()
+{
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    alert(1)
+    $.ajax({
+        contentType: JSON,
+        data: { 'email': email, 'password': password },
+        url: "loginurl",
+        error: function () { alert('an error occured please try again later'); },
+        success: function (data) {
+            if (data.d === 'Success') {
+                window.localStorage.setItem('email', email);
+                window.location.href = "#MakeList";
+            }
+            else { alert("Wrong email or password try again.") }
+        };
+    });
+};
