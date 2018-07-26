2ddab2b6cdd032ff2661b6bb501ed148ea65edf4
Liat Yashin <Liatyashin@mail.tau.ac.il>
2018-07-26 10:33:31 +0300

    login and register

--- dly_web_app_2/dly_web_app_2/JavaScript.js
+++ dly_web_app_2/dly_web_app_2/JavaScript.js
@@ -1,19 +1,43 @@
 ﻿function login()
 {
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
-    alert(1)
+    var username = document.getElementById("username_l");
+    var password = document.getElementById("password_l");
+    var params = JSON.stringify({ 'username': username, 'password': password });
+    alert(params);
     $.ajax({
         contentType: JSON,
-        data: { 'email': email, 'password': password },
-        url: "loginurl",
+        data: JSON.stringify({'parameters': params}),
+        url: "https://menageusers.azurewebsites.net/api/CheckUserPassword?code=DHfAvU1gxX9j3AxwXgye3f20A/5mNCxWgi2iKyUvZm1iNAzhmOPdtQ==",
+        type: "POST",
         error: function () { alert('an error occured please try again later'); },
         success: function (data) {
             if (data.d === 'Success') {
-                window.localStorage.setItem('email', email);
+                window.localStorage.setItem('username', username);
                 window.location.href = "#MakeList";
             }
-            else { alert("Wrong email or password try again.") }
-        };
+            else { alert(data.d); }
+        }
     });
-};
+}
+
+function register() {
+    var name = document.getElementById("name_r");
+    var username = document.getElementById("username_r");
+    var password = document.getElementById("password_r");
+    var params = JSON.stringify({ 'username': username, 'name': name, 'password': password });
+    alert(1);
+    $.ajax({
+        contentType: JSON,
+        data: JSON.stringify({ 'parameters': params }),
+        url: "https://menageusers.azurewebsites.net/api/CreateNewUser",
+        type: "POST",
+        error: function () { alert('an error occured please try again later'); },
+        success: function (data) {
+            if (data.d === 'Success') {
+                window.localStorage.setItem('username', username);
+                window.location.href = "#MakeList";
+            }
+            else { alert(data.d); }
+        }
+    });
+}
