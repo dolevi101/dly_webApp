be9a34aed1a6fa2fccb3478bf0d304f8f7430ccf
Liat Yashin <Liatyashin@mail.tau.ac.il>
2018-07-30 20:47:05 +0300

    finished login,register

--- dly_web_app_2/dly_web_app_2/JavaScript1.js
+++ dly_web_app_2/dly_web_app_2/JavaScript1.js
@@ -1,47 +1,89 @@
-﻿function login() {
-    var username = document.getElementById("username_l").value;
-    var password = document.getElementById("password_l").value;
-    var params = JSON.stringify({ 'username': username, 'password': password });
-    alert(params);
+﻿function checkNotEmpty(idList) {
+    for (let id of idList) {
+        if ($("#" + id).val() === "" || $("#" + id).val() === null) {
+            alert("all fields must be filled.");
+            return false;
+        }
+    }
+    return true;
+}
+
+function login() {
+    if (!checkNotEmpty(["username_l", "password_l"]))
+        return false;
+    var username = $("#username_l").val();
+    var password = $("#password_l").val();
+    //var params = JSON.stringify({ 'username': username, 'password': password });
     $.ajax({
         contentType: JSON,
-        beforeSend: function (request) {
-            request.setRequestHeader("Access-Control-Allow-Origin", authorizationToken);
-        },
         //data: JSON.stringify({ 'parameters': params }),
-        url: "https://menageusers.azurewebsites.net/api/CheckUserPassword?code=DHfAvU1gxX9j3AxwXgye3f20A/5mNCxWgi2iKyUvZm1iNAzhmOPdtQ==&parameters={%27username%27:%20%27" + username + "%27,%20%27password%27:%27" + password + "%27}",
-        //url: "https://menageusers.azurewebsites.net/api/CheckUserPassword",
-        //"https://changecartposition.azurewebsites.net/api/HttpTriggerCSharp1?code=cM5bXc0uMoDzQ6ZyDeLpJaNDJhzldRAutUMXX7RqxprrpGaTgUY4iQ==&name={%27position%27:%20%27"+ cartPosition +"%27,%20%27id%27:%27"+ cartID +"%27}"
-        type: "POST",
+        url: "https://manageuser1.azurewebsites.net/api/CheckUserPassword?code=2iiKpSILpGlmte7kFrmhoylGJbLacEVVQw9K/z5hXzN2k/Oprv8LVg==&parameters={%27username%27:%20%27" + username + "%27,%20%27password%27:%27" + password + "%27}",
+        type: "GET",
         error: function () { alert('an error occured please try again later'); },
         success: function (data) {
-            if (data.d === 'Success') {
+            if (data === 'Success') {
                 window.localStorage.setItem('username', username);
                 window.location.href = "#makeList";
             }
-            else { alert(data.d); }
+            else { alert(data); }
+        }
+    });
+}
+
+function logout() {
+    localStorage.removeItem('username');
+    window.location.href = "#home";
+}
+
+function isUsernameOK() {
+    var username = $("#username_r").val();
+    if (username === "" || username === null)
+        return;
+    var notOK = "<center> username already taken...</center>";
+    var OK = "<center> username is OK</center>";
+  //var params = JSON.stringify({ 'username': username});
+    $.ajax({
+        contentType: JSON,
+        //data: JSON.stringify({ 'parameters': params }),
+        url: "https://manageuser1.azurewebsites.net/api/isUsernameOK?code=35GCQ4W2iySJ/hYjQs38Dmh9R3aEYNqPtFwCqMDOOn5MC8/UHQzS5w==&parameters={%27username%27:%20%27" + username + "%27}",
+        type: "GET",
+        error: function () { alert('an error occured please try again later'); },
+        success: function (data) {
+            if (data === 'Success') {
+                $("#isUsernameOK").html(OK);
+            }
+            else {
+                $("#isUsernameOK").html(notOK);
+            }
         }
     });
 }
 
 function register() {
-    var name = document.getElementById("name_r").value;
-    var username = document.getElementById("username_r").value;
-    var password = document.getElementById("password_r").value;
+    if (!checkNotEmpty(["username_r", "password_r", "name_r"]))
+        return false;
+    var isOK = $("#isUsernameOK").html();
+    var OK = "<center> username is OK</center>";
+    if (isOK !== OK) {
+        alert("problem with username.");
+        return false;
+    }
+    var name = $("#name_r").val();
+    var username = $("#username_r").val();
+    var password = $("#password_r").val();
     var params = JSON.stringify({ 'username': username, 'name': name, 'password': password });
-    alert(1);
     $.ajax({
         contentType: JSON,
         data: JSON.stringify({ 'parameters': params }),
-        url: "https://menageusers.azurewebsites.net/api/CreateNewUser",
-        type: "POST",
+        url: "https://manageuser1.azurewebsites.net/api/CreateNewUser?code=a3TvjtUk6/wx3rSFj30skJ/Jyd/IE1HemHn1H2Mle0UhjM8kFHshRg==&parameters={%27username%27:%20%27" + username + "%27,%20%27password%27:%27" + password + "%27,%20%27name%27:%27" + name + "%27}",
+        type: "GET",
         error: function () { alert('an error occured please try again later'); },
         success: function (data) {
-            if (data.d === 'Success') {
+            if (data === 'Success') {
                 window.localStorage.setItem('username', username);
                 window.location.href = "#makeList";
             }
-            else { alert(data.d); }
+            else { alert(data); }
         }
     });
 }
@@ -59,4 +101,16 @@ function oldLists(username) {
         }
     });
     $("#oldListsTable").text(table);
+}
+
+function showUsername(id) {
+    var username_line="";
+    if (localStorage.username) {
+        username_line += '<center><a style="font-size:large; color:white;" onclick="logout();">Hi ';
+        username_line += localStorage.username;
+        username_line += '. logout?</a></center>';
+        $(id).html(username_line);
+    }
+    else
+        $(id).text(" ");
 }
